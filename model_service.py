1958d63a4546107e92729895d7f17a837d700b25
wijaya eka <wijayaeka2014@gmail.com>
2025-02-26 23:12:43 +0700

    update new model implementation 2

--- model_service.py
+++ model_service.py
@@ -13,7 +13,7 @@ import os
 # Konfigurasi Azure OpenAI
 client_openai = openai.AzureOpenAI(
     azure_endpoint="https://your-azure-openai-instance.openai.azure.com/",  # Ganti dengan endpoint Azure kamu
-    api_key="b98cbbc0f92946588a1f164c728a4e14",  # Ganti dengan API Key kamu
+    api_key="xxx",  # Ganti dengan API Key kamu
     api_version="2023-12-01-preview"  # Sesuaikan dengan versi API yang digunakan
 )
 DEPLOYMENT_NAME = "gpt-4"
@@ -27,7 +27,7 @@ DEPLOYMENT_NAME = "gpt-4"
 from groq import Groq
 
 client = Groq(
-    api_key="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",
+    api_key="xxx",
 )
 class TextClassifier:
     def __init__(self, model_name="captainrobotfly/ioc_v2", mapping_file="results/model3/model_new/mapping.json", hf_api_key="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"):
